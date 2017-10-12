037ce51d6d87d0e96c664fccb895e77f067ea87d
0xk175un3 <0xk175un3@gmail.com>
2017-10-12 17:34:54 +0300

    devhouse eslint style refactoring

--- args-manager.js
+++ args-manager.js
@@ -1,98 +1,97 @@
 'use strict';
 
-const path = require('path');
-
-const COLLECTION_NAME = "migrations";
-const CONFIG_CONNECTION = "db.connectionString";
-const MIGRATIONS_DIR = "./migrations";
+const path              = require('path'),
+      COLLECTION_NAME   = 'migrations',
+      CONFIG_CONNECTION = 'db.connectionString',
+      MIGRATIONS_DIR    = './migrations';
 
 
 class ArgsManager {
 
-    constructor(args, config, processPath) {
-        this.args = args;
-        this.config = config;
-        this.processPath = processPath;
+  constructor(args, config, processPath) {
+    this.args        = args;
+    this.config      = config;
+    this.processPath = processPath;
+  }
+
+  static removeQuotes(string) {
+    return string.replace(/"/g, '').replace(/'/g, '');
+  }
+
+  /**
+   * look up the argument in the argument list
+   * @param {string} argName the name of the argument
+   * @returns {boolean} true of argument found, false otherwise
+   */
+  findArg(argName) {
+    let found = false;
+
+    for (let i = 0; i < this.args.length; i++) {
+
+      if (this.args[i].includes('=')) { //if --blala=pathto  format
+        found = this.args[i].includes(argName) || found;
+      } else {
+        found = this.args[i].trim() === argName || found;
+      }
+
     }
 
-    static removeQuotes(string) {
-        return string.replace(/"/g, '').replace(/'/g, '');
-    }
+    return found;
+  }
 
-    /**
-     * look up the argument in the argument list
-     * @param argName {string} the name of the argument
-     * @returns {boolean} true of argument found, false otherwise
-     */
-    findArg(argName) {
-        let found = false;
+  findKey(argName) {
+    for (let i = 0; i < this.args.length; i++) {
+      if (this.args[i].includes('=') && this.args[i].includes(argName)) {
 
-        for (let i = 0; i < this.args.length; i++) {
+        let arg = this.args[i].replace(argName + '=', '');
+        return ArgsManager.removeQuotes(arg);
 
-            if (this.args[i].includes("=")) { //if --blala=pathto  format
-                found = this.args[i].includes(argName) || found;
-            } else {
-                found = this.args[i].trim() === argName || found;
-            }
+      } else if (this.args[i] === argName) {
 
-        }
-
-        return found;
-    }
-
-    findKey(argName) {
-        for (let i = 0; i < this.args.length; i++) {
-            if (this.args[i].includes("=") && this.args[i].includes(argName)) {
-
-                let arg = this.args[i].replace(argName + "=", "");
-                return ArgsManager.removeQuotes(arg);
-
-            } else if (this.args[i] === argName) {
-
-                if (i + 1 === this.args.length) {
-                    return undefined;
-                } else {
-                    if (this.args[i + 1].includes('--')) {
-                        return undefined;
-                    }
-                    return ArgsManager.removeQuotes(this.args[i + 1]);
-                }
-
-            }
-        }
-    }
-
-
-    getConnectionString() {
-        let configArg = this.findKey("--configConnection"),
-            connectionString;
-
-        if (configArg) {
-            connectionString = this.config.get(configArg);
+        if (i + 1 === this.args.length) {
+          return undefined;
         } else {
-            connectionString = this.config.get(CONFIG_CONNECTION);
+          if (this.args[i + 1].includes('--')) {
+            return undefined;
+          }
+          return ArgsManager.removeQuotes(this.args[i + 1]);
         }
 
-        return connectionString;
+      }
+    }
+  }
+
+
+  getConnectionString() {
+    let configArg = this.findKey('--configConnection'),
+      connectionString;
+
+    if (configArg) {
+      connectionString = this.config.get(configArg);
+    } else {
+      connectionString = this.config.get(CONFIG_CONNECTION);
     }
 
-    migrationsFilesPath() {
-        let migrationsArg = this.findKey("--migrationsDir");
+    return connectionString;
+  }
 
-        if (migrationsArg) {
-            return path.resolve(this.processPath, migrationsArg);
-        }
+  migrationsFilesPath() {
+    let migrationsArg = this.findKey('--migrationsDir');
 
-        return path.resolve(this.processPath, MIGRATIONS_DIR);
+    if (migrationsArg) {
+      return path.resolve(this.processPath, migrationsArg);
     }
 
-    getCollectionName() {
-        let collectionArg = this.findKey("--collection");
-        if (collectionArg) {
-            return collectionArg
-        }
-        return COLLECTION_NAME;
+    return path.resolve(this.processPath, MIGRATIONS_DIR);
+  }
+
+  getCollectionName() {
+    let collectionArg = this.findKey('--collection');
+    if (collectionArg) {
+      return collectionArg;
     }
+    return COLLECTION_NAME;
+  }
 }
 
 module.exports = ArgsManager;
--- migration.js
+++ migration.js
@@ -1,252 +1,253 @@
-const fs = require('fs'),
-    path = require('path');
+'use strict';
+
+const fs   = require('fs'),
+      path = require('path');
 
 
 function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function copyFile(source, target) {
-    return new Promise(function (resolve, reject) {
-        let rd = fs.createReadStream(source);
-        rd.on('error', rejectCleanup);
-        let wr = fs.createWriteStream(target);
-        wr.on('error', rejectCleanup);
+  return new Promise(function(resolve, reject) {
+    let rd = fs.createReadStream(source);
+    rd.on('error', rejectCleanup);
+    let wr = fs.createWriteStream(target);
+    wr.on('error', rejectCleanup);
 
-        function rejectCleanup(err) {
-            rd.destroy();
-            wr.end();
-            reject(err);
-        }
+    function rejectCleanup(err) {
+      rd.destroy();
+      wr.end();
+      reject(err);
+    }
 
-        wr.on('finish', resolve);
-        rd.pipe(wr);
-    });
+    wr.on('finish', resolve);
+    rd.pipe(wr);
+  });
 }
 
 class Migration {
-    constructor(name, path, applied) {
-        if (!name || (!path && !applied)) {
-            throw ('PARSERROR')
-        }
-        this.name = name;
-        this.path = path;
-        this.applied = applied;
+  constructor(name, path, applied) {
+    if (!name || (!path && !applied)) {
+      throw ('PARSERROR');
+    }
+    this.name    = name;
+    this.path    = path;
+    this.applied = applied;
 
-        if (this.path) {
-            this.file = require(this.path);
-        }
+    if (this.path) {
+      this.file = require(this.path);
+    }
+  }
+
+  async up() {
+    let randomColour = getRandomIntInclusive(32, 36);
+    console.time(`${this.name}`);
+    console.log(`\x1b[${randomColour}m up migration ${this.name} started\x1b[0m`);
+    let result = await this.file.up();
+    console.timeEnd(`${this.name}`);
+    console.log(`\x1b[${randomColour}m up migration ${this.name} finished\x1b[0m`);
+    return result;
+  }
+
+  async down() {
+    let randomColour = getRandomIntInclusive(32, 36);
+    console.time(`${this.name}`);
+    console.log(`\x1b[${randomColour}m down migration ${this.name} started\x1b[0m`);
+    let result = await this.file.down();
+    console.timeEnd(`${this.name}`);
+    console.log(`\x1b[${randomColour}m down migration ${this.name} finished\x1b[0m`);
+    return result;
+  }
+
+  get isApplied() {
+    return !!this.applied;
+  }
+
+  get timeStamp() {
+    let regex = /^[0-9]+(?!-)/;
+    return +regex.exec(this.name)[0];
+  }
+
+  static getMigrationsFromFolder(argsManager) {
+    try {
+      let migrationDir = argsManager.migrationsFilesPath(),
+        files = fs.readdirSync(migrationDir);
+
+
+      return files.map(file => {
+        let fullPath = path.resolve(migrationDir, file);
+        return new Migration(file, fullPath);
+      });
+    } catch (err) {
+      if (err.code === 'ENOENT') {
+        throw ('it looks like migration folder could not be found. Please use --migrationsDir option and "help" for mode details');
+      } else {
+        throw (err);
+      }
+    }
+  }
+
+  isEqual(to) {
+    return this.timeStamp === to.timeStamp &&
+      this.name === to.name;
+  }
+
+  static compare(a, b) {
+    if (a.timeStamp < b.timeStamp) {
+      return -1;
+    }
+    if (a.timeStamp > b.timeStamp) {
+      return 1;
+    }
+    return 0;
+  }
+
+  static sortMigrationsArray(array) {
+    return array.sort(Migration.compare);
+  }
+
+  /**
+   * produce descend on migrations set down to name (inclusively)
+   * @param migrations {Array<Migration>} array of migrations
+   * @param downToName {string} name of the migration to descent there
+   * @returns {Array<Migration>} array of migration to be downgraded
+   */
+  static descentMigrations(migrations, downToName) {
+    let result = [],
+        found  = Migration.findMigrationInSet(migrations, downToName);
+
+    if (downToName === undefined) {
+      found = {
+        index: 0
+      };
     }
 
-    async up() {
-        let randomColour = getRandomIntInclusive(32, 36);
-        console.time(`${this.name}`);
-        console.log(`\x1b[${randomColour}m up migration ${this.name} started\x1b[0m`);
-        let result = await this.file.up();
-        console.timeEnd(`${this.name}`);
-        console.log(`\x1b[${randomColour}m up migration ${this.name} finished\x1b[0m`);
-        return result;
+    if (!found) {
+      throw ({
+        message: `provided migration name ${downToName} could not be found`
+      });
     }
 
-    async down() {
-        let randomColour = getRandomIntInclusive(32, 36);
-        console.time(`${this.name}`);
-        console.log(`\x1b[${randomColour}m down migration ${this.name} started\x1b[0m`);
-        let result = await this.file.down();
-        console.timeEnd(`${this.name}`);
-        console.log(`\x1b[${randomColour}m down migration ${this.name} finished\x1b[0m`);
-        return result;
+
+    for (let i = migrations.length - 1; i >= found.index; i--) {
+      result.push(migrations[i]);
     }
 
-    get isApplied() {
-        return !!this.applied;
+    return result;
+  }
+
+  /**
+   * produce ascent on migration up to the specific name (inclusively)
+   * @param migrations {Array<Migration>} array of migrations
+   * @param upToName {string} name of the migration to descent there
+   * @returns {Array<Migration>} array of migration to be applied
+   */
+  static ascentMigrations(migrations, upToName) {
+    let result = [],
+        found  = Migration.findMigrationInSet(migrations, upToName);
+
+    if (upToName === undefined) {
+      found = {
+        index: migrations.length - 1
+      };
     }
 
-    get timeStamp() {
-        let regex = /^[0-9]+(?!-)/;
-        return +regex.exec(this.name)[0];
+    if (!found) {
+      throw ({
+        message: `provided migration name ${upToName} could not be found`
+      });
     }
 
-    static getMigrationsFromFolder(argsManager) {
-        try {
-            let migrationDir = argsManager.migrationsFilesPath(),
-                files = fs.readdirSync(migrationDir);
 
-
-            return files.map(file => {
-                let fullPath = path.resolve(migrationDir, file);
-                return new Migration(file, fullPath)
-            });
-        } catch (err) {
-            if (err.code === 'ENOENT') {
-                throw ('it looks like migration folder could not be found. Please use --migrationsDir option and "help" for mode details')
-            } else {
-                throw (err)
-            }
-        }
+    for (let i = 0; i <= found.index; i++) {
+      result.push(migrations[i]);
     }
 
-    isEqual(to) {
-        return this.timeStamp === to.timeStamp &&
-            this.name === to.name;
+    return result;
+  }
+
+  static findMigrationInSet(set, migration) {
+    for (let i = 0; i < set.length; i++) {
+      if (typeof migration === 'string') {
+        if (set[i].name === migration) {
+          return {
+            index: i,
+            migration: set[i]
+          };
+        }
+
+      } else if (migration instanceof Migration) {
+        if (set[i].isEqual(migration)) {
+          return {
+            index: i,
+            migration: set[i]
+          };
+        }
+      }
+    }
+  }
+
+  static intersectMigrationsSets(a, b) {
+    let result = [];
+
+    for (let migration of a) {
+      if (Migration.findMigrationInSet(b, migration)) {
+        result.push(migration);
+      }
     }
 
-    static compare(a, b) {
-        if (a.timeStamp < b.timeStamp) {
-            return -1;
+    return result;
+  }
+
+  static subtractMigrationsSets(minuend, deduction) {
+    let result = [];
+    for (let migration of minuend) {
+      let contains = false;
+
+      for (let toFind of deduction) {
+        if (toFind.isEqual(migration)) {
+          contains = true;
+          break;
         }
-        if (a.timeStamp > b.timeStamp) {
-            return 1;
-        }
-        return 0;
+      }
+
+      if (!contains) {
+        result.push(migration);
+      }
     }
 
-    static sortMigrationsArray(array) {
-        return array.sort(Migration.compare)
+    return Migration.sortMigrationsArray(result);
+  }
+
+  static createMigration(argsManager) {
+    let name       = argsManager.findKey('create', ''),
+      file         = '' + Date.now(),
+      templatePath = path.resolve(__dirname, './migration-template.js');
+
+    if (name !== undefined) {
+      file += '-' + name;
     }
 
-    /**
-     * produce descend on migrations set down to name (inclusively)
-     * @param migrations {Array<Migration>} array of migrations
-     * @param downToName {string} name of the migration to descent there
-     * @returns {Array<Migration>} array of migration to be downgraded
-     */
-    static descentMigrations(migrations, downToName) {
-        let result = [],
-            found = Migration.findMigrationInSet(migrations, downToName);
+    file += '.js';
 
-        if (downToName === undefined) {
-            found = {
-                index: 0
-            }
-        }
-
-        if (!found) {
-            throw ({
-                message: `provided migration name ${downToName} could not be found`
-            })
-        }
-
-
-        for (let i = migrations.length - 1; i >= found.index; i--) {
-            result.push(migrations[i]);
-        }
-
-        return result;
+    if (!fs.existsSync(argsManager.migrationsFilesPath())) {
+      fs.mkdirSync(argsManager.migrationsFilesPath());
     }
 
-    /**
-     * produce ascent on migration up to the specific name (inclusively)
-     * @param migrations {Array<Migration>} array of migrations
-     * @param upToName {string} name of the migration to descent there
-     * @returns {Array<Migration>} array of migration to be applied
-     */
-    static ascentMigrations(migrations, upToName) {
-        let result = [],
-            found = Migration.findMigrationInSet(migrations, upToName);
-
-        if (upToName === undefined) {
-            found = {
-                index: migrations.length - 1
-            };
-        }
-
-        if (!found) {
-            throw ({
-                message: `provided migration name ${upToName} could not be found`
-            })
-        }
-
-
-        for (let i = 0; i <= found.index; i++) {
-            result.push(migrations[i]);
-        }
-
-        return result;
+    try {
+      let filePath = path.resolve(argsManager.migrationsFilesPath(), file);
+      return copyFile(templatePath, filePath)
+        .then(result => {
+          console.log(`file ${file} successfully created`);
+          return result;
+        });
+    } catch (err) {
+      console.error(`could not create ${file}:`, JSON.stringify(err, null, 2));
     }
-
-    static findMigrationInSet(set, migration) {
-        for (let i = 0; i < set.length; i++) {
-            if (typeof migration === 'string') {
-                if (set[i].name === migration) {
-                    return {
-                        index: i,
-                        migration: set[i]
-                    }
-                }
-
-            } else if (migration instanceof Migration) {
-                if (set[i].isEqual(migration)) {
-                    return {
-                        index: i,
-                        migration: set[i]
-                    }
-                }
-            }
-        }
-    }
-
-    static intersectMigrationsSets(a, b) {
-        let result = [];
-
-        for (let migration of a) {
-            if (Migration.findMigrationInSet(b, migration)) {
-                result.push(migration);
-            }
-        }
-
-        return result;
-    }
-
-    static subtractMigrationsSets(minuend, deduction) {
-        let result = [];
-        for (let migration of minuend) {
-            let contains = false;
-
-            for (let toFind of deduction) {
-                if (toFind.isEqual(migration)) {
-                    contains = true;
-                    break;
-                }
-            }
-
-            if (!contains) {
-                result.push(migration);
-            }
-        }
-
-        return Migration.sortMigrationsArray(result);
-    }
-
-    static createMigration(argsManager) {
-        let name = argsManager.findKey("create", ""),
-            file = '' + Date.now(),
-            templatePath = path.resolve(__dirname, './migration-template.js');
-
-        if (name !== undefined) {
-            file += '-' + name;
-        }
-
-        file += '.js';
-
-        if (!fs.existsSync(argsManager.migrationsFilesPath())) {
-            fs.mkdirSync(argsManager.migrationsFilesPath());
-        }
-
-        try {
-            let filePath = path.resolve(argsManager.migrationsFilesPath(), file);
-            return copyFile(templatePath, filePath)
-                .then(result => {
-                    console.log(`file ${file} successfully created`);
-                    return result;
-                })
-        } catch (err) {
-            console.error(`could not create ${file}:`, JSON.stringify(err, null, 2));
-        }
-
-    }
+  }
 }
 
 
